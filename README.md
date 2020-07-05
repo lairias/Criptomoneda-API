c9eb7fdd9348633d7c9117004ce0204fda3d4115
lairias <lairias@unah.hn>
2020-07-05 14:11:43 -0600

    Errores en las instanicas

---
+++ README.md
@@ -0,0 +1,8 @@
+### APlicacion de cotizaciones de la criptoMoneda,
+### tiene complicaciones en la que viene las instancias,
+### Convertidad de Webpack y babael por las Clases de la aplicacion,
+
+
+----
+recomendaciones es de comprobarlos con los cdn, que se encuentran dentro de la carpera src/js, cambiarlos en el archivo index.html
+----
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: [ "./src/js/app.js"],
+  output: {
+      filename:'bundler.js',
+      path: path.join(__dirname,'/dist')
+  },
+  module:{
+      rules:[
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use:{loader:'babel-loader'}
+          },
+          {
+              test: /\.css$/,
+              use:[
+                  {loader:'style-loader'},
+                  {loader:'css-loader'}
+              ]
+          }
+      ]
+  }
+
+};
